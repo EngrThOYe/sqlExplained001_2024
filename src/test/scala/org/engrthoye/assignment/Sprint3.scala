a849c047cf7bb367f80a8e1a1bc917c08ddbd134
EngrThOYe <adetoy_4real@yahoo.com>
2024-03-12 12:13:25 +0000

    some TODO comment added to sprint3 task

--- src/test/scala/org/engrthoye/assignment/Sprint3.scala
+++ src/test/scala/org/engrthoye/assignment/Sprint3.scala
@@ -5,7 +5,10 @@ import org.junit.Test
 
 
 class Sprint3 extends SparkBase{
-
+  //TODO
+  // change example_1 _2 and _3 to task_1 _2 _3
+  // apply .show(false) at the end of the query to display your query output Line 19, 30, 43
+  // optional, implement the Illustration in the video under the illustration.sprint3 yo can copy it from engrthoye branch
   @Test
   def examplle_1(): Unit = {
     readParquetTable("MTcars")
