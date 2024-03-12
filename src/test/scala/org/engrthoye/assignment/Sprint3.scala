77d9032fb18bb21251f4110c4a3727e354ff21da
Lawal Khadijat <ladeholar@gmail.com>
2024-03-12 13:50:10 +0100

    corrections made

--- src/test/scala/org/engrthoye/assignment/Sprint3.scala
+++ src/test/scala/org/engrthoye/assignment/Sprint3.scala
@@ -10,35 +10,35 @@ class Sprint3 extends SparkBase{
   // apply .show(false) at the end of the query to display your query output Line 19, 30, 43
   // optional, implement the Illustration in the video under the illustration.sprint3 yo can copy it from engrthoye branch
   @Test
-  def examplle_1(): Unit = {
+  def task_1(): Unit = {
     readParquetTable("MTcars")
     spark.sql(
 
       "SELECT disp from MTcars"
 
-    )
+    ).show(false)
 
   }
 
   @Test
-  def examplle_2(): Unit = {
+  def task_2(): Unit = {
     readParquetTable("MTcars")
     spark.sql(
 
       "SELECT hp, wt, am from MTcars"
 
-    )
+    ).show(false)
 
   }
 
   @Test
-  def examplle_3(): Unit = {
+  def task_3(): Unit = {
     readParquetTable("MTcars")
     spark.sql(
 
       "SELECT * from MTcars"
 
-    )
+    ).show(false)
 
   }
 
