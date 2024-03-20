56b66bbb0949bb5c1ac3564a7b4108f5d5f3c042
Olukunle Balogun <balogunolukunleabidemi@gmail.com>
2024-03-20 18:18:40 +0000

    minor update to SparkBase

--- src/test/scala/org/engrthoye/illustration/Sprint3.scala
+++ src/test/scala/org/engrthoye/illustration/Sprint3.scala
@@ -7,13 +7,35 @@ import org.junit.Test
 class Sprint3 extends SparkBase{
 
   @Test
-  def examplle_1(): Unit = {
+  def example_1(): Unit = {
     readParquetTable("MTcars")
     spark.sql(
 
-      ""
+      "SELECT disp FROM MTCars"
 
-    )
+    ).show(false)
+
+  }
+
+  @Test
+  def example_2(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT hp, wt, am FROM MTCars"
+
+    ).show(false)
+
+  }
+
+  @Test
+  def example_3(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT * FROM MTCars"
+
+    ).show(false)
 
   }
 
