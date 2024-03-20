fc16451bf0ba3248777e724ca144e3a7960290f6
EngrThOYe <adetoy_4real@yahoo.com>
2024-03-20 19:35:26 +0000

    minor update to SparkBase

--- src/test/scala/org/engrthoye/SparkBase.scala
+++ src/test/scala/org/engrthoye/SparkBase.scala
@@ -3,7 +3,7 @@ package org.engrthoye
 abstract class SparkBase extends SparkConfig {
 
   def readParquetTable(tableName: String) = {
-    val dataFrame = spark.read.parquet(s"/Users/mo_baller_g/IdeaProjects/sqlExplained001_2024/src/test/resources/tableserver/parquet/$tableName.parquet")
+    val dataFrame = spark.read.parquet(getClass.getResource(s"/tableserver/parquet/$tableName.parquet").getPath.replace("%20"," "))
     dataFrame.createOrReplaceTempView(tableName)
     dataFrame.show(false)
   }
