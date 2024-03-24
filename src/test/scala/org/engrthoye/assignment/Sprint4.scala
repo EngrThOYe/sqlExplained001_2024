0c5a95702e23231b06d540eb14795fa374d9d211
apple <adepojuakinloluferanmi@gmail.com>
2024-03-24 02:22:08 +0100

    new update

---
+++ src/test/scala/org/engrthoye/assignment/Sprint4.scala
@@ -0,0 +1,59 @@
+package org.engrthoye.assignment
+
+import org.engrthoye.SparkBase
+import org.junit.Test
+
+
+class Sprint4 extends SparkBase{
+
+  @Test
+  def task_1(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT disp FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def task_2(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def task_3(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def task_4(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear, disp, gear, drat, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def task_5(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT * FROM MTcars"
+
+    ).show(false)
+  }
+
+}
