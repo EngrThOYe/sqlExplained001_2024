37d7f247cda999f029d33d3b1316893aea2f1660
Lawal Khadijat <ladeholar@gmail.com>
2024-03-24 22:30:02 +0100

    sprint 4

---
+++ src/test/scala/org/engrthoye/assignment/Sprint4.scala
@@ -0,0 +1,42 @@
+package org.engrthoye.assignment
+
+import org.engrthoye.SparkBase
+import org.junit.Test
+
+
+class Sprint4 extends SparkBase{
+
+  @Test
+  def task_1(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT disp from MTcars"
+
+    ).show(false)
+
+  }
+
+  @Test
+  def task_2(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT hp, wt, am from MTcars"
+
+    ).show(false)
+
+  }
+
+  @Test
+  def task_3(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT * from MTcars"
+
+    ).show(false)
+
+  }
+
+}
