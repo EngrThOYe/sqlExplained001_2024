e6d0a3e348cfa8126e3c5fd0c13446945c0e9307
apple <adepojuakinloluferanmi@gmail.com>
2024-03-24 21:59:07 +0100

    new update

--- src/test/scala/org/engrthoye/illustration/Sprint3.scala
+++ src/test/scala/org/engrthoye/illustration/Sprint3.scala
@@ -7,14 +7,53 @@ import org.junit.Test
 class Sprint3 extends SparkBase{
 
   @Test
-  def examplle_1(): Unit = {
+  def example_1(): Unit = {
     readParquetTable("MTcars")
     spark.sql(
 
-      ""
+      "SELECT disp FROM MTcars"
 
     ).show(false)
+  }
 
+  @Test
+  def example_2(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def example_3(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def example_4(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT model, gear, disp, gear, drat, gear FROM MTcars"
+
+    ).show(false)
+  }
+
+  @Test
+  def example_5(): Unit = {
+    readParquetTable("MTcars")
+    spark.sql(
+
+      "SELECT * FROM MTcars"
+
+    ).show(false)
   }
 
 }
